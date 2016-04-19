4922d77be62f14be58e8cea4b3868808fc38d8f7
James McClain <jmcclain@azavea.com>
2016-04-19 11:47:54 -0400

    More Work on Mapshed Geoprocessing

--- summary/src/main/scala/MapshedJob.scala
+++ summary/src/main/scala/MapshedJob.scala
@@ -8,23 +8,30 @@ import org.apache.spark.SparkContext
 import spark.jobserver.{SparkJob, SparkJobValid, SparkJobValidation}
 
 
-trait MapshedJobParams { }
+trait MapshedJobParams
 
-case class RasterJobParams(geometry: Seq[MultiPolygon], layerId: String) extends MapshedJobParams
-case class RasterVectorJobParams(geometry: Seq[MultiPolygon], layerId: String) extends MapshedJobParams
+case class RasterVectorJobParams(
+  polygon: Seq[MultiPolygon],
+  vector: GeometryCollection,
+  rasterLayerId: LayerId
+) extends MapshedJobParams
 
 object MapshedJob extends SparkJob with JobUtils {
+
   override def validate(sc: SparkContext, config: Config): SparkJobValidation = {
     // TODO Add real validation
     SparkJobValid
   }
 
   override def runJob(sc: SparkContext, config: Config): Any = {
-    val params = parseConfig(config)
-    val extent = GeometryCollection(params.geometry).envelope
-    val nlcdLayer = queryAndCropLayer(catalog(sc), params.nlcdLayerId, extent)
-
-    histogram(nlcdLayer, params.geometry)
+     parseConfig(config) match {
+      case RasterVectorJobParams(polygon, vector, rasterLayerId) =>
+        val extent = GeometryCollection(polygon).envelope
+        val rasterLayer = queryAndCropLayer(catalog(sc), rasterLayerId, extent)
+         // rasterVectorJoin(vector, rasterLayer)
+         println(s"XXX $vector")
+      case _ => throw new Exception("Unknown Job Type")
+    }
   }
 
   def parseConfig(config: Config): MapshedJobParams = {
@@ -33,27 +40,39 @@ object MapshedJob extends SparkJob with JobUtils {
 
     val getOptional = getOptionalFn(config)
 
-    val zoom = config.getInt("input.zoom")
-    val rasterLayer = LayerId(config.getString("input.raster"), zoom)
+    config.getString("input.operationType") match {
+      case "RasterVectorJoin" =>
+        val rasterCRS = getOptional("input.rasterCRS") match {
+          case Some("LatLng") => LatLng
+          case Some("WebMercator") => WebMercator
+          case Some("ConsuAlbers") => ConusAlbers
+          case _ => ConusAlbers
+        }
+        val polygonCRS = getOptional("input.polygonCRS") match {
+          case Some("LatLng") => LatLng
+          case Some("WebMercator") => WebMercator
+          case Some("ConusAlbers") => ConusAlbers
+          case _ => LatLng
+        }
+        val vectorCRS = getOptional("input.vectorCRS") match {
+          case Some("LatLng") => LatLng
+          case Some("WebMercator") => WebMercator
+          case Some("ConusAlbers") => ConusAlbers
+          case _ => LatLng
+        }
+        val polygon = config.getStringList("input.polygon").asScala.map {
+          str => parseGeometry(str, polygonCRS, rasterCRS)
+        }
+        val vector = config.getStringList("input.vector").asScala.map {
+          str => parseGeometry(str, vectorCRS, rasterCRS)
+        }
+        val zoom = config.getInt("input.zoom")
+        val rasterLayerId = LayerId(config.getString("input.raster"), zoom)
 
-    val rasterCRS = getOptional("input.rasterCRS") match {
-      case Some("LatLng") => LatLng
-      case Some("WebMercator") => WebMercator
-      case Some("ConsuAlbers") => ConusAlbers
-      case _ => ConusAlbers
-    }
-    val polygonCRS = getOptional("input.polygonCRS") match {
-      case Some("LatLng") => LatLng
-      case Some("WebMercator") => WebMercator
-      case Some("ConusAlbers") => ConusAlbers
-      case _ => LatLng
-    }
+        RasterVectorJobParams(polygon, vector, rasterLayerId)
 
-    val geometry = config.getStringList("input.polygon").asScala.map {
-      str => parseGeometry(str, polygonCRS, rasterCRS)
+      case _ => throw new Exception("Unknown Job Type")
     }
-
-    MapshedJobParams(nlcdLayer, geometry)
   }
 
   def histogram(layer: TileLayerRDD[SpatialKey], multiPolygons: Seq[MultiPolygon]) = {
