cf8892414ab3a8155b783a5e14c23821ef8afdfd
James McClain <jmcclain@azavea.com>
2016-04-22 11:12:22 -0400

    Add R-Tree based raster & line histogram
     * Given a polygon, a list of vectors, and a raster layer id, will
       return the histogram of the intersection of the three.
     * Since the list of vectors can be very large, we first compose a range
       tree to store which vectors are present in which tiles, and then
       query the tree for each tile, and then for each vector in the tile
       capture the value of the raster at intersection points.
     * Two sample requests have been added, one using the NHDFlowline data
       set, and the other using the DRB data set in a small polygon. A much
       larger DRB sample can be found here: https://goo.gl/YFvX3F (20 MB).
     * Currently we only handle a vector list containing LineStrings, but
       the actual data set may include Geometry and MultiLineStrings in
       addition.

--- summary/src/main/scala/MapshedJob.scala
+++ summary/src/main/scala/MapshedJob.scala
@@ -1,105 +1,101 @@
 package org.wikiwatershed.mmw.geoprocessing
 
-import geotrellis.spark.{LayerId, SpatialKey, TileLayerRDD}
-import geotrellis.vector.{GeometryCollection, MultiPolygon, MultiPolygonResult, Polygon, PolygonResult}
-import com.typesafe.config.Config
+import geotrellis.raster._
 import geotrellis.raster.rasterize.{Callback, Rasterizer}
+import geotrellis.spark.{LayerId, SpatialKey, TileLayerRDD}
+import geotrellis.vector._
+import geotrellis.vector.io._
+
+import com.typesafe.config.Config
+import com.vividsolutions.jts.geom.Envelope
+import com.vividsolutions.jts.index.strtree.STRtree
 import org.apache.spark.SparkContext
 import spark.jobserver.{SparkJob, SparkJobValid, SparkJobValidation}
+import spray.json._
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 
-case class MapshedJobParams(nlcdLayerId: LayerId, geometry: Seq[MultiPolygon])
+sealed trait MapshedJobParams
+
+case class RasterLinesJobParams(
+  polygon: Seq[MultiPolygon],
+  lines: Seq[Line],
+  rasterLayerId: LayerId
+) extends MapshedJobParams
 
 object MapshedJob extends SparkJob with JobUtils {
+
   override def validate(sc: SparkContext, config: Config): SparkJobValidation = {
     // TODO Add real validation
     SparkJobValid
   }
 
   override def runJob(sc: SparkContext, config: Config): Any = {
-    val params = parseConfig(config)
-    val extent = GeometryCollection(params.geometry).envelope
-    val nlcdLayer = queryAndCropLayer(catalog(sc), params.nlcdLayerId, extent)
+     parseConfig(config) match {
+      case RasterLinesJobParams(polygon, lines, rasterLayerId) =>
+        val extent = GeometryCollection(polygon).envelope
+        val rasterLayer = queryAndCropLayer(catalog(sc), rasterLayerId, extent)
 
-    histogram(nlcdLayer, params.geometry)
+        rasterLinesJoin(rasterLayer, lines)
+
+      case _ =>
+         throw new Exception("Unknown Job Type")
+    }
   }
 
   def parseConfig(config: Config): MapshedJobParams = {
-    import scala.collection.JavaConverters._
-    import geotrellis.proj4.{LatLng, WebMercator}
+    val crs: String => geotrellis.proj4.CRS = getCRS(config, _)
 
-    val getOptional = getOptionalFn(config)
+    config.getString("input.operationType") match {
+      case "RasterLinesJoin" =>
+        val rasterCRS = crs("input.rasterCRS")
+        val polygonCRS = crs("input.polygonCRS")
+        val linesCRS = crs("input.vectorCRS")
+        val polygon = config.getStringList("input.polygon").asScala.map({ str => parseGeometry(str, polygonCRS, rasterCRS) })
+        val lines = config.getStringList("input.vector").asScala.map({ str => str.parseJson.convertTo[Line].reproject(linesCRS, rasterCRS) })
+        val zoom = config.getInt("input.zoom")
+        val rasterLayerId = LayerId(config.getString("input.raster"), zoom)
 
-    val zoom = config.getInt("input.zoom")
-    val nlcdLayer = LayerId(config.getString("input.nlcdLayer"), zoom)
+        RasterLinesJobParams(polygon, lines, rasterLayerId)
 
-    val tileCRS = getOptional("input.tileCRS") match {
-      case Some("LatLng") => LatLng
-      case Some("WebMercator") => WebMercator
-      case Some("ConsuAlbers") => ConusAlbers
-      case _ => ConusAlbers
+      case _ =>
+        throw new Exception("Unknown Job Type")
     }
-    val polyCRS = getOptional("input.polyCRS") match {
-      case Some("LatLng") => LatLng
-      case Some("WebMercator") => WebMercator
-      case Some("ConusAlbers") => ConusAlbers
-      case _ => LatLng
-    }
-
-    val geometry = config.getStringList("input.geometry").asScala.map {
-      str => parseGeometry(str, polyCRS, tileCRS)
-    }
-
-    MapshedJobParams(nlcdLayer, geometry)
   }
 
-  def histogram(layer: TileLayerRDD[SpatialKey], multiPolygons: Seq[MultiPolygon]) = {
-    import scala.collection.mutable
-    import geotrellis.raster.RasterExtent
+  def rasterLinesJoin(rasterLayer: TileLayerRDD[SpatialKey], lines: Seq[Line]): Map[Int, Int] = {
+    val rtree = new STRtree
 
-    val histogramParts = layer.map { case (key, tile) =>
-      multiPolygons.map { multiPolygon =>
-        val extent = layer.metadata.mapTransform(key) // transform spatial key to extent
-        val rasterExtent = RasterExtent(extent, tile.cols, tile.rows) // transform extent to raster extent
-        val clipped = multiPolygon & extent
-        val localHistogram = mutable.Map.empty[Int, Int]
+    lines.foreach({lineString =>
+      val Extent(xmin, ymin, xmax, ymax) = lineString.envelope
+      rtree.insert(new Envelope(xmin, xmax, ymin, ymax), lineString)
+    })
 
-        def intersectionComponentsToHistogram(ps: Seq[Polygon]) = {
-          ps.foreach { p =>
-            Rasterizer.foreachCellByPolygon(p, rasterExtent)(
-              new Callback {
-                def apply(col: Int, row: Int): Unit = {
-                  val nlcdType = tile.get(col, row)
-
-                  if (!localHistogram.contains(nlcdType)) {
-                    localHistogram(nlcdType) = 0
-                  }
-
-                  localHistogram(nlcdType) += 1
-                }
-              }
-            )
+    rasterLayer
+      .map({ case (key, tile) => {
+        val extent = rasterLayer.metadata.mapTransform(key)
+        val Extent(xmin, ymin, xmax, ymax) = extent
+        val rasterExtent = RasterExtent(extent, tile.cols, tile.rows)
+        val pixels = mutable.ListBuffer.empty[(Int, Int)]
+        val cb = new Callback {
+          def apply(col: Int, row: Int): Unit = {
+            val pixel = (col, row)
+            pixels += pixel
           }
         }
 
-        clipped match {
-          case PolygonResult(p) => intersectionComponentsToHistogram(List(p))
-          case MultiPolygonResult(mp) => intersectionComponentsToHistogram(mp.polygons)
-          case _ =>
-        }
+        rtree.query(new Envelope(xmin, xmax, ymin, ymax)).asScala.foreach({ lineStringObject =>
+          Rasterizer.foreachCellByLineString(lineStringObject.asInstanceOf[Line], rasterExtent)(cb)
+        })
 
-        localHistogram.toMap
-      }
-    }
-
-    histogramParts.reduce { (s1, s2) =>
-      (s1 zip s2).map { case (left, right) =>
-        (left.toSeq ++ right.toSeq)
-          .groupBy(_._1)
-          .map { case (nlcdType, counts) =>
-            (nlcdType, counts.map(_._2).sum)
-          }
-      }
-    }
+        pixels
+          .distinct.map({ case (col, row) => tile.get(col, row) })
+          .groupBy(identity).map({ case (k: Int, list: mutable.ListBuffer[Int]) => (k -> list.length) })
+          .toList
+      }})
+      .reduce({ (left, right) => left ++ right})
+      .groupBy(_._1).map({ case (k: Int, list: List[(Int, Int)]) => (k -> list.map(_._2).sum) })
   }
 }
