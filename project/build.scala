0bc913df5640f2099918d13630c354aedc4d99f3
Kelly Innes <kinnes@azavea.com>
2017-08-21 12:40:11 -0400

    3.0.0-alpha-2

--- project/build.scala
+++ project/build.scala
@@ -30,7 +30,7 @@ object Geoprocessing extends Build {
     super.settings ++
   Seq(
     shellPrompt := { s => Project.extract(s).currentProject.id + " > " },
-    version := "3.0.0-alpha",
+    version := "3.0.0-alpha-2",
     scalaVersion := Version.scala,
     organization := "org.wikiwatershed.mmw.geoprocessing",
     name := "mmw-geoprocessing",
