8061332974a06b64988eaccaa127edb632161903
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2020-01-31 17:00:46 -0500

    4.0.3

--- project/build.scala
+++ project/build.scala
@@ -30,7 +30,7 @@ object Geoprocessing extends Build {
     super.settings ++
   Seq(
     shellPrompt := { s => Project.extract(s).currentProject.id + " > " },
-    version := "4.0.2",
+    version := "4.0.3",
     scalaVersion := Version.scala,
     organization := "org.wikiwatershed.mmw.geoprocessing",
     name := "mmw-geoprocessing",
