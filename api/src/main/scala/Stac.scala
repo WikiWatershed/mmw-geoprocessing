89652f811c9970e1a646b2acf7b6144eb8ac6ed6
Terence Tuhinanshu <terence@tuhinanshu.com>
2024-05-16 17:16:29 -0400

    WIP

--- api/src/main/scala/Stac.scala
+++ api/src/main/scala/Stac.scala
@@ -5,18 +5,16 @@ import cats.effect.unsafe.IORuntime
 import cats.syntax.functor._
 import cats.syntax.nested._
 import cats.syntax.option._
-import com.azavea.stac4s.{Bbox, TwoDimBbox}
-import com.azavea.stac4s.api.client.{SearchFilters, SttpStacClient}
-import geotrellis.proj4.{LatLng, WebMercator}
+import com.azavea.stac4s._
+import com.azavea.stac4s.api.client._
+import geotrellis.proj4._
 import geotrellis.raster.effects.MosaicRasterSourceIO
-import geotrellis.raster.{MosaicRasterSource, StringName}
-import geotrellis.stac.raster.{StacAssetRasterSource, StacItemAsset}
-import geotrellis.vector.Extent
+import geotrellis.stac.raster._
+import geotrellis.vector._
 import sttp.client3.UriContext
 import sttp.client3.akkahttp._
 
-import scala.concurrent.{Await, Future}
-import geotrellis.vector.MultiPolygon
+import scala.concurrent._
 
 trait Stac extends Utils {
   import scala.concurrent.ExecutionContext.Implicits.global
@@ -25,18 +23,28 @@ trait Stac extends Utils {
     val uri = uri"https://api.impactobservatory.com/stac-aws"
 
     val aoi = parseGeometry(shape.shape, LatLng, LatLng)
-    val extent = Extent(aoi.getEnvelopeInternal())
-    val bbox = TwoDimBbox(extent.xmin, extent.ymin, extent.xmax, extent.ymax)
 
-    val searchFilters = SearchFilters(bbox=Some(bbox))
-//    val searchFilters = SearchFilters()
+//    val searchFilters = SearchFilters(bbox=Some(bbox))
+    val searchFilters = SearchFilters(
+      collections=List("io-10m-annual-lulc"),
+      intersects=Some(aoi),
+//      query=Map(
+//        "start_datetime" -> GreaterThanEqual(Json.withString("2023-01-01T00:00:00Z")),
+//        "end_datetime" -> LessThanEqual(Json.withString("2024-01-01T00:00:00Z")),
+//      )
+//      datetime="2023-01-01T00:00:00Z/2023-12-31T23:59:59Z"
+//      datetime=Some(
+//        TemporalExtent(
+//          Instant.parse("2023-01-01T00:00:00Z"),
+//          Instant.parse("2024-01-01T00:00:00Z")))
+    )
     val limit = 100
     val assetName = "supercell".r
     val withGDAL = false
     val defaultCRS = WebMercator
     val parallelMosaicEnable = false
-    val collectionName = StringName("io-10m-annual-lulc")
-//    val extent = Extent(0, 0, 180, 180)
+//    val extent = Extent(-180, -90, 180, 90)
+//    val searchFilters = SearchFilters(bbox=Some(TwoDimBbox(extent.xmin, extent.ymin, extent.xmax, extent.ymax)))
 
     val backend = AkkaHttpBackend()
     val client = SttpStacClient(backend, uri)
@@ -45,13 +53,19 @@ trait Stac extends Utils {
       .search(searchFilters)
       .take(limit)
       .compileToFutureList
-      .map(MosaicRasterSource.fromStacItems(collectionName, _, assetName, defaultCRS, withGDAL, parallelMosaicEnable))
+//      .map(MosaicRasterSource.fromStacItems(collectionName, _, assetName, defaultCRS, withGDAL, parallelMosaicEnable))
 
-    val result = source.nested.map(_.read(extent)).value.map(_.flatten).map {
-      case Some(raster) => s"raster.extent = ${raster.extent}"
-      case None => s"no rasters found in extent ${extent}"
+    source.map {
+      _.map(_.id).mkString(",")
     }
 
-    result
+//    futureInt
+
+//    val result = source.nested.map(_.read(extent)).value.map(_.flatten).map {
+//      case Some(raster) => s"raster.extent = ${raster.extent}"
+//      case None => s"no rasters found in extent ${extent}"
+//    }
+//
+//    result
   }
 }
