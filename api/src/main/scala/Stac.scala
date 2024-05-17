6c519ddbfba7d71ed7415cdac015b2d00d800c27
Terence Tuhinanshu <terence@tuhinanshu.com>
2024-05-16 22:05:57 -0400

    Query pixel counts

--- api/src/main/scala/Stac.scala
+++ api/src/main/scala/Stac.scala
@@ -7,13 +7,16 @@ import cats.syntax.nested._
 import cats.syntax.option._
 import com.azavea.stac4s._
 import com.azavea.stac4s.api.client._
+import eu.timepit.refined.types.all.NonEmptyString
 import geotrellis.proj4._
+import geotrellis.raster._
 import geotrellis.raster.effects.MosaicRasterSourceIO
 import geotrellis.stac.raster._
 import geotrellis.vector._
 import sttp.client3.UriContext
 import sttp.client3.akkahttp._
 
+import java.time.Instant
 import scala.concurrent._
 
 trait Stac extends Utils {
@@ -25,18 +28,20 @@ trait Stac extends Utils {
     val aoi = parseGeometry(shape.shape, LatLng, LatLng)
 
 //    val searchFilters = SearchFilters(bbox=Some(bbox))
+    val collectionName = StringName("io-10m-annual-lulc")
     val searchFilters = SearchFilters(
-      collections=List("io-10m-annual-lulc"),
+      collections=List(collectionName.value),
       intersects=Some(aoi),
+//      query=Map("start_datetime" -> List(GreaterThanEqual(JString("2023-01-01T00:00:00Z"))))
 //      query=Map(
 //        "start_datetime" -> GreaterThanEqual(Json.withString("2023-01-01T00:00:00Z")),
 //        "end_datetime" -> LessThanEqual(Json.withString("2024-01-01T00:00:00Z")),
 //      )
 //      datetime="2023-01-01T00:00:00Z/2023-12-31T23:59:59Z"
-//      datetime=Some(
-//        TemporalExtent(
-//          Instant.parse("2023-01-01T00:00:00Z"),
-//          Instant.parse("2024-01-01T00:00:00Z")))
+      datetime=Some(
+        TemporalExtent(
+          Instant.parse("2023-01-02T00:00:00Z"),
+          Instant.parse("2024-01-01T00:00:00Z")))
     )
     val limit = 100
     val assetName = "supercell".r
@@ -53,18 +58,36 @@ trait Stac extends Utils {
       .search(searchFilters)
       .take(limit)
       .compileToFutureList
-//      .map(MosaicRasterSource.fromStacItems(collectionName, _, assetName, defaultCRS, withGDAL, parallelMosaicEnable))
+      .map(MosaicRasterSource.fromStacItems(collectionName, _, assetName, defaultCRS, withGDAL, parallelMosaicEnable))
 
-    source.map {
-      _.map(_.id).mkString(",")
-    }
+//    source.map {
+//      _.map(_.id).mkString(",")
+//    }
 
 //    futureInt
 
-//    val result = source.nested.map(_.read(extent)).value.map(_.flatten).map {
-//      case Some(raster) => s"raster.extent = ${raster.extent}"
-//      case None => s"no rasters found in extent ${extent}"
-//    }
+//    val tiffCRS: Future[CRS] = source.map { _.map { _.crs } getOrElse WebMercator }
+
+//    source
+//      .map {
+//        _.map { _.crs.toString } mkString "," }
+
+    source
+      .nested
+      .map { rs => rs.read(aoi.reproject(LatLng, rs.crs).extent) }
+      .value
+      .map { _.flatten }
+      .map { _.map { _.tile.size } mkString "," }
+
+//    val result = source
+//      .nested
+//      .map(_.read(aoi.extent))
+//      .value
+//      .map(_.flatten)
+//      .map {
+//        case Some(raster) => s"raster.extent = ${raster.extent}"
+//        case None => s"no rasters found in extent ${aoi.extent}"
+//      }
 //
 //    result
   }
